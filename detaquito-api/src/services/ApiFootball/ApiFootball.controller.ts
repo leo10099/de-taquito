a2ada4d9e0625ac607558b8216645599f4323290
leo10099 <leandrohanc@gmail.com>
2020-07-16 00:35:45 -0300

    Add api-football endpoints

--- detaquito-api/src/services/ApiFootball/ApiFootball.controller.ts
+++ detaquito-api/src/services/ApiFootball/ApiFootball.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 // Components
 import { ApiFootballService } from './ApiFootball.service';
@@ -8,7 +8,27 @@ export class ApiFootballController {
   constructor(private readonly apiFootball: ApiFootballService) {}
 
   @Get('/countries')
-  async getCountries() {
+  async countries() {
     return await this.apiFootball.getCountries();
+  }
+
+  @Get('/seasons')
+  async seasons() {
+    return await this.apiFootball.getSeasons();
+  }
+
+  @Get('league/:league')
+  async league(@Param() param: { league: string }) {
+    return await this.apiFootball.getLeague(param.league);
+  }
+
+  @Get('/season/:season')
+  async season(@Param() param: { season: string }) {
+    return await this.apiFootball.getSeason(param.season);
+  }
+
+  @Get('standings/:league')
+  async stadings(@Param() param: { league: string }) {
+    return await this.apiFootball.getStandings(param.league);
   }
 }
--- detaquito-api/src/services/ApiFootball/ApiFootball.service.ts
+++ detaquito-api/src/services/ApiFootball/ApiFootball.service.ts
@@ -1,14 +1,53 @@
 // Services
 import { HttpService, Injectable, HttpException } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class ApiFootballService {
-  constructor(private httpService: HttpService, private configService: ConfigService) {}
+  constructor(private httpService: HttpService) {}
 
   async getCountries() {
     try {
       const { data } = await this.httpService.get('/countries').toPromise();
+      return data.api;
+    } catch (e) {
+      const errorMessage = e.response.data?.message || e.response.statusText;
+      throw new HttpException(errorMessage, e.response.status);
+    }
+  }
+
+  async getSeasons() {
+    try {
+      const { data } = await this.httpService.get('/seasons').toPromise();
+      return data.api;
+    } catch (e) {
+      const errorMessage = e.response.data?.message || e.response.statusText;
+      throw new HttpException(errorMessage, e.response.status);
+    }
+  }
+
+  async getLeague(id: string) {
+    try {
+      const { data } = await this.httpService.get(`/leagues/league/${id}`).toPromise();
+      return data.api;
+    } catch (e) {
+      const errorMessage = e.response.data?.message || e.response.statusText;
+      throw new HttpException(errorMessage, e.response.status);
+    }
+  }
+
+  async getSeason(season: string) {
+    try {
+      const { data } = await this.httpService.get(`/leagues/season/${season}`).toPromise();
+      return data.api;
+    } catch (e) {
+      const errorMessage = e.response.data?.message || e.response.statusText;
+      throw new HttpException(errorMessage, e.response.status);
+    }
+  }
+
+  async getStandings(league: string) {
+    try {
+      const { data } = await this.httpService.get(`/leagueTable/${league}`).toPromise();
       return data.api;
     } catch (e) {
       const errorMessage = e.response.data?.message || e.response.statusText;
