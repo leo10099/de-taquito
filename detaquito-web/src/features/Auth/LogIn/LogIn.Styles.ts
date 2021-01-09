0dd57b4d74065cf0c3cba329cdab028edc355612
leo10099 <leandrohanc@gmail.com>
2021-01-09 13:59:26 -0300

    Layout fixes

--- detaquito-web/src/features/Auth/LogIn/LogIn.Styles.ts
+++ detaquito-web/src/features/Auth/LogIn/LogIn.Styles.ts
@@ -3,7 +3,6 @@ import { FlexColumnCentered } from "utils/styles";
 
 export const LogInContainer = styled.main`
 	${FlexColumnCentered()};
-	width: 100vw;
 `;
 
 export const LogInFormContent = styled.div`
--- detaquito-web/src/providers/FadeInFadeOutProvider.tsx
+++ detaquito-web/src/providers/FadeInFadeOutProvider.tsx
@@ -5,9 +5,9 @@ const FadeInFadeOutProvider: React.FC = ({ children }) => {
 	return (
 		<motion.div
 			initial={{ opacity: 0, height: "100vh", overflow: "hidden" }}
-			animate={{ opacity: 1, height: "100vh", overflow: "visible" }}
+			animate={{ opacity: 1, height: "100%", overflow: "visible" }}
 			exit={{ opacity: 0, height: "100vh", overflow: "hidden" }}
-			transition={{ duration: 0.2 }}
+			transition={{ duration: 1 }}
 		>
 			{children}
 		</motion.div>
